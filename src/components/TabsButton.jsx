27ead7f0c94eb2e7c7c02c3ecaef1248f8cd1cf6
vipuls11 <vipulvishwakarma161@gmail.com>
2025-02-09 10:40:56 +0530

    tabsbutton

--- src/components/TabsButton.jsx
+++ src/components/TabsButton.jsx
@@ -39,15 +39,15 @@ const TabsButton = () => {
   return (
     <div className='w-[90%] m-auto py-20'>
         <h2 className='text-4xl font-semibold mb-10'>Experience</h2> 
-        <div className='flex gap-10'>
-            <div className='flex flex-col w-58 border border-white p-2 shadow'>
+        <div className='lg:flex md:flex gap-10'>
+            <div className='lg:flex md:flex flex-col lg:w-58 md:w-58 border border-white p-2 shadow'>
                 {
                     jobs.map((item, index)=>{
-                        return <button key={item.id} onClick={()=>setValue(index)} className={`job-btn w-52 uppercase  ${index === value && 'active-btn'}`} >{item.company}</button>
+                        return <button key={item.id} onClick={()=>setValue(index)} className={`job-btn lg:w-52 md:52 w-full uppercase  ${index === value && 'active-btn'}`} >{item.company}</button>
                     })
                 }
             </div>
-            <div>
+            <div className='lg:mt-0 mt-5'>
                 <h2 className='text-3xl font-semibold mb-5 text-purple-400'>{title}</h2>
                 <p className='text-2xl mb-3'>{dates}</p>
                 {/* <p> <MdOutlineKeyboardDoubleArrowRight/> {duties}</p> */}
